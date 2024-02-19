75cd2b48b4a96030d72a27bf549ed2ce94dfc3e9
Devinix00 <dpldpl123456@naver.com>
2024-02-19 17:40:41 +0900

    json placeholder api 통신 테스트 코드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <main></main>
-    </>
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/"
+        );
+
+        const jsonData = await response.json();
+        console.log(jsonData);
+      } catch (error) {
+        console.log("fetchData 에러 발생:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,8 +1,44 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import App from "../src/App";
 
-test("main 태그가 있어요?", () => {
+const mockTodoList = Array(200)
+  .fill("")
+  .map((_, index) => ({
+    userId: 1,
+    id: index + 1,
+    title: `Test Todo ${index + 1}`,
+    completed: false,
+  }));
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve(mockTodoList),
+  })
+) as jest.Mock;
+
+test("API 경로 'https://jsonplaceholder.typicode.com/todos/'를 호출하면 200개의 배열 데이터가 성공적으로 받아와지는지 테스트합니다", async () => {
   render(<App />);
-  const mainElement = screen.getByRole("main");
-  expect(mainElement).toBeInTheDocument();
+  await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+
+  expect(fetch).toHaveBeenCalledWith(
+    "https://jsonplaceholder.typicode.com/todos/"
+  );
+
+  const responseData = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/"
+  ).then((response) => response.json());
+  expect(Array.isArray(responseData)).toBe(true);
+  expect(responseData.length).toBe(200);
+
+  expect(responseData[0]).toEqual({
+    userId: 1,
+    id: 1,
+    title: "Test Todo 1",
+    completed: false,
+  });
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
